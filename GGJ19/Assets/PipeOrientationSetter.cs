3280975fa9df3f112d4d557b52fc7fd601bcf588
Damien <damien.murtagh@glu.com>
2019-01-26 10:52:35 -0800

    Added some basic pipe section classes

---
+++ GGJ19/Assets/PipeOrientationSetter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Class to keep the orientation of pipes correct in the editor
+[ExecuteInEditMode]
+public class PipeOrientationSetter : MonoBehaviour
+{
+    private PipeSection pipeSection;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        pipeSection = gameObject.GetComponent<PipeSection>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (pipeSection.Orientation)
+        {
+            case PipeSection.PipeRotation.NoRotation:
+                gameObject.transform.rotation = Quaternion.identity;
+                break;
+            case PipeSection.PipeRotation.Clockwise90:
+                gameObject.transform.rotation = Quaternion.Euler(0, 0, 90);
+                break;
+            case PipeSection.PipeRotation.Clockwise180:
+                gameObject.transform.rotation = Quaternion.Euler(0, 0, 180);
+                break;
+            case PipeSection.PipeRotation.Clockwise270:
+                gameObject.transform.rotation = Quaternion.Euler(0, 0, 270);
+                break;
+        }
+    }
+}
---
+++ GGJ19/Assets/Scripts/PipeSection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSection : MonoBehaviour
+{
+    public enum PipeRotation
+    {
+        NoRotation,
+        Clockwise90,
+        Clockwise180,
+        Clockwise270
+    };
+    
+    public PipeSection.PipeRotation Orientation = PipeRotation.NoRotation; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

be8b7a87fd9032fe19d21964bad9d52027599353
Damien <damien.murtagh@glu.com>
2019-01-27 00:09:37 -0800

    Added beginnings of the black hole

---
+++ GGJ19/Assets/Scripts/BlackHole.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHole : MonoBehaviour
+{
+    private float m_RotationsPerSecond = 0.1f;
+    private float m_ElapsedTime = 0.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_ElapsedTime = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_ElapsedTime += Time.deltaTime;
+        gameObject.transform.rotation = Quaternion.Euler(0, 0, (360 * m_RotationsPerSecond * m_ElapsedTime) % 360);
+    }
+}

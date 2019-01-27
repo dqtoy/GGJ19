498af30c2068d302f20a85605193d7106691e5a2
Siyuan <finalspace.1031@gmail.com>
2019-01-27 12:37:13 -0800

    win lose scene

---
+++ GGJ19/Assets/Scripts/Lose.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lose : MonoBehaviour
+{
+    public FadeInOut item;
+
+	private void Start()
+	{
+        item.Play();
+	}
+}
+
+
---
+++ GGJ19/Assets/Scripts/Win.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour
+{
+    public FadeInOut item;
+
+    private void Start()
+    {
+        item.Play();
+    }
+}
